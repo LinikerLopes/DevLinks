222304369d0d84c277bbbc316affd61f269a56ef
Canjica <linicker11@gmail.com>
2024-09-10 12:10:24 -0300

    Projeto finalizado, falta alterar só tag img no js

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    //trocar imagem
+    
+   // const img = document.querySelector("#profile img")
+
+   /* if(html.classList.contains('light')){
+      img.setAttribute('src', "./assets/")
+    } else{
+        img.setAttribute("src", "./assets/avatar.png")
+    }*/
+
+   
+
+   //troca da imagem
+
+   //pegar a tag
+
+   //
+}
+
